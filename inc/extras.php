20d19b3f37ece789fbb01591c125fbfe92c34fef
Sam Smith <samcbr600@gmail.com>
2017-08-18 18:03:05 -0400

    Styles for the index, and blog pages. The styles are targeting the article, and the aside. Check out the diffs to see what was done

--- inc/extras.php
+++ inc/extras.php
@@ -22,7 +22,7 @@ function humescores_body_classes( $classes ) {
 	// Adds a class of hfeed to non-singular pages.
 	if ( ! is_singular() ) {
 		$classes[] = 'hfeed';
-                $classes[] = 'archive-view';
+		$classes[] = 'archive-view';
 	}
 	
 	// Add a class telling us if the sidebar is in use.

bf94a094fb98db07f0c01454fb35046deebef415
Sam Smith <samcbr600@gmail.com>
2017-08-22 10:02:06 -0400

    this is going to be the changes for both 13.01 and 13.02, look for the responsive images in functions.php, and everything else is going to be the second video. This next one was a freakin doozy. You are going to need to watch the video, and maybe the other course on SVG wed icons for this one. mostly try and figure out how to call in the function to place your icon wherever you want it, and take a look at the functionality in the footer menu. All you do is type in the url, and it will display the correct icon. maybe the coolest thing in the world.

--- footer.php
+++ footer.php
@@ -16,19 +16,20 @@
 	<?php get_sidebar( 'footer' ); ?>
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<?php
-		// Make sure there is a social menu to display.
-		if ( has_nav_menu( 'social' ) ) { ?>
-		<nav class="social-menu">
-		<?php wp_nav_menu( array( 'theme_location' => 'social' ) ); ?>
-		</nav><!-- .social-menu -->
-		<?php } ?>
+		<div class="site-footer__wrap">
+			<?php
+			// Make sure there is a social menu to display.
+			if ( has_nav_menu( 'social' ) ) { ?>
+			<nav class="social-menu">
+				<?php wp_nav_menu( array( 'theme_location' => 'social' ) ); ?>
+			</nav><!-- .social-menu -->
+			<?php } ?>
 
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'humescores' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'humescores' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'humescores' ), 'humescores', '<a href="https://mor10.com/courses" rel="designer">Morten Rand-Hendriksen</a>' ); ?>
-		</div><!-- .site-info -->
+			<div class="site-info">
+				<div><a href="<?php echo esc_url( __( 'https://wordpress.org/', 'humescores' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'humescores' ), 'WordPress' ); ?></a></div>
+				<div><?php printf( esc_html__( 'Theme: %1$s by %2$s', 'humescores' ), 'humescores', '<a href="https://mor10.com/courses" rel="designer">Morten Rand-Hendriksen</a>' ); ?></div>
+			</div><!-- .site-info -->
+		</div><!-- .site-footer__wrap -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
--- functions.php
+++ functions.php
@@ -42,7 +42,7 @@ function humescores_setup() {
 	 */
 	add_theme_support( 'post-thumbnails' );
 	add_image_size( 'humescores-full-bleed', 2000, 1200, true );
-	add_image_size( 'humescores-index-img', 800, 450, true );
+	add_image_size( 'humescores-index-img', 1000, 550, true );
 
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(
@@ -67,14 +67,14 @@ function humescores_setup() {
 		'default-color' => 'ffffff',
 		'default-image' => '',
 	) ) );
-	
+
 	// Add theme support for Custom Logo
 	add_theme_support( 'custom-logo', array(
 		'width' => 90,
 		'height' => 90,
 		'flex-width' => true,
 	));
-	
+
 }
 endif;
 add_action( 'after_setup_theme', 'humescores_setup' );
@@ -95,16 +95,16 @@ function humescores_fonts_url() {
 	$pt_serif = _x( 'on', 'PT Serif font: on or off', 'humescores' );
 
 	$font_families = array();
-	
+
 	if ( 'off' !== $source_sans_pro ) {
 		$font_families[] = 'Source Sans Pro:400,400i,700,900';
 	}
-	
+
 	if ( 'off' !== $pt_serif ) {
 		$font_families[] = 'PT Serif:400,400i,700,700i';
 	}
-	
-	
+
+
 	if ( in_array( 'on', array($source_sans_pro, $pt_serif) ) ) {
 
 		$query_args = array(
@@ -151,6 +151,81 @@ function humescores_content_width() {
 }
 add_action( 'after_setup_theme', 'humescores_content_width', 0 );
 
+
+/**
+ * Add custom image sizes attribute to enhance responsive image functionality
+ * for content images.
+ *
+ * @origin Twenty Seventeen 1.0
+ *
+ * @param string $sizes A source size value for use in a 'sizes' attribute.
+ * @param array  $size  Image size. Accepts an array of width and height
+ *                      values in pixels (in that order).
+ * @return string A source size value for use in a content image 'sizes' attribute.
+ */
+function humescores_content_image_sizes_attr( $sizes, $size ) {
+	$width = $size[0];
+
+	if ( 900 <= $width ) {
+		$sizes = '(min-width: 900px) 700px, 900px';
+	}
+
+	if ( is_active_sidebar( 'sidebar-1' ) || is_active_sidebar( 'sidebar-2' ) ) {
+		$sizes = '(min-width: 900px) 600px, 900px';
+	}
+
+	return $sizes;
+}
+add_filter( 'wp_calculate_image_sizes', 'humescores_content_image_sizes_attr', 10, 2 );
+
+/**
+ * Filter the `sizes` value in the header image markup.
+ *
+ * @origin Twenty Seventeen 1.0
+ *
+ * @param string $html   The HTML image tag markup being filtered.
+ * @param object $header The custom header object returned by 'get_custom_header()'.
+ * @param array  $attr   Array of the attributes for the image tag.
+ * @return string The filtered header image HTML.
+ */
+function humescores_header_image_tag( $html, $header, $attr ) {
+	if ( isset( $attr['sizes'] ) ) {
+		$html = str_replace( $attr['sizes'], '100vw', $html );
+	}
+	return $html;
+}
+add_filter( 'get_header_image_tag', 'humescores_header_image_tag', 10, 3 );
+
+/**
+ * Add custom image sizes attribute to enhance responsive image functionality
+ * for post thumbnails.
+ *
+ * @origin Twenty Seventeen 1.0
+ *
+ * @param array $attr       Attributes for the image markup.
+ * @param int   $attachment Image attachment ID.
+ * @param array $size       Registered image size or flat array of height and width dimensions.
+ * @return string A source size value for use in a post thumbnail 'sizes' attribute.
+ */
+function humescores_post_thumbnail_sizes_attr( $attr, $attachment, $size ) {
+
+	if ( !is_singular() ) {
+		if ( is_active_sidebar( 'sidebar-1' ) ) {
+			$attr['sizes'] = '(max-width: 900px) 90vw, 800px';
+		} else {
+			$attr['sizes'] = '(max-width: 1000px) 90vw, 1000px';
+		}
+	} else {
+		$attr['sizes'] = '100vw';
+	}
+
+	return $attr;
+}
+add_filter( 'wp_get_attachment_image_attributes', 'humescores_post_thumbnail_sizes_attr', 10, 3 );
+
+
+
+
 /**
  * Register widget area.
  *
@@ -166,7 +241,7 @@ function humescores_widgets_init() {
 		'before_title'  => '<h2 class="widget-title">',
 		'after_title'   => '</h2>',
 	) );
-	
+
 	register_sidebar( array(
 		'name'          => esc_html__( 'Page Sidebar', 'humescores' ),
 		'id'            => 'sidebar-2',
@@ -176,7 +251,7 @@ function humescores_widgets_init() {
 		'before_title'  => '<h2 class="widget-title">',
 		'after_title'   => '</h2>',
 	) );
-	
+
 	register_sidebar( array(
 		'name'          => esc_html__( 'Footer Widgets', 'humescores' ),
 		'id'            => 'footer-1',
@@ -186,7 +261,7 @@ function humescores_widgets_init() {
 		'before_title'  => '<h2 class="widget-title">',
 		'after_title'   => '</h2>',
 	) );
-	
+
 }
 add_action( 'widgets_init', 'humescores_widgets_init' );
 
@@ -196,7 +271,7 @@ add_action( 'widgets_init', 'humescores_widgets_init' );
 function humescores_scripts() {
 	// Enqueue Google Fonts: Source Sans Pro and PT Serif
 	wp_enqueue_style( 'humescores-fonts', humescores_fonts_url() );
-	
+
 	wp_enqueue_style( 'humescores-style', get_stylesheet_uri() );
 
 	wp_enqueue_script( 'humescores-navigation', get_template_directory_uri() . '/js/navigation.js', array('jquery'), '20151215', true );
@@ -206,7 +281,7 @@ function humescores_scripts() {
 	));
 
 	wp_enqueue_script( 'humescores-functions', get_template_directory_uri() . '/js/functions.js', array('jquery'), '20161201', true );
-	
+
 	wp_enqueue_script( 'humescores-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
@@ -239,3 +314,8 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+/**
+ * Load SVG icon functions.
+ */
+require get_template_directory() . '/inc/icon-functions.php';
